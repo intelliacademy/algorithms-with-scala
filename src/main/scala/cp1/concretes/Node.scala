ede1c0b926406b785281f072d4074a698c96dc0a
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-04-07 19:12:55 +0400

    Paragraph_String_Operations

--- src/main/scala/cp1/concretes/Node.scala
+++ src/main/scala/cp1/concretes/Node.scala
@@ -3,19 +3,23 @@ package cp1.concretes
 
 import java.lang.Comparable
 
-class Node[T <: Comparable[T]] extends Comparable[Node[T]] {
-
-  def this(value: T) = {
-    this()
-  }
+class Node[T <: Comparable[T]](val value:T,var left: Node[T],var right: Node[T]) extends Comparable[Node[T]] {
   
+  def this(value:T) = this(value,null,null)
   
-  def compareTo(o: Node[T]): Int = {
-    65
-  }
+  override def compareTo(o: Node[T]): Int = this.value.compareTo(o.value)
+  
 }
 
+class NilNode[T <: Comparable[T]] extends Node[T](null.asInstanceOf[T],null,null) {
+  
+  override def compareTo(o: Node[T]): Int = {
+    if(o.isInstanceOf[NilNode[T]]) 0
+    else -1
+  }
+  
+}
 
-class NilNode extends Node[Nothing] {
-  override def compareTo(o: Node[Nothing]): Int = 0
+object NilNode {
+  def apply[T <: Comparable[T]] = new NilNode[T]
 }
--- src/main/scala/cp1/package.scala
+++
@@ -1,5 +0,0 @@
-package com.intellibucket.lessons
-
-package object cp1 {
-
-}
