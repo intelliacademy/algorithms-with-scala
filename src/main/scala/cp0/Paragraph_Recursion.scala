a5977b84f24b46356de20e93f00eb2caed0c7957
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-04-07 06:54:16 +0400

    first look

--- src/main/scala/cp0/Paragraph_Recursion.scala
+++ src/main/scala/cp0/Paragraph_Recursion.scala
@@ -3,7 +3,7 @@ package cp0
 
 import scala.annotation.tailrec
 
-object Paragraph_Recursion extends App{
+object Paragraph_Recursion extends App {
   def factorial(n: Int): Int = {
     @tailrec
     def recFactorial(n: Int, acc: Int) : Int = {
@@ -13,6 +13,10 @@ object Paragraph_Recursion extends App{
     recFactorial(n, 1)
   }
 
+  /*
+  n = 5 -> recFactorial(5, 1) -> recFactorial(4, 5) -> recFactorial(3, 20) -> recFactorial(2, 60) -> recFactorial(1, 120) -> 120
+  n = 4 -> recFactorial(4, 1) -> recFactorial(3, 4) -> recFactorial(2, 12) -> recFactorial(1, 24) -> 24
+   */
 
   println(factorial(5))
 }
