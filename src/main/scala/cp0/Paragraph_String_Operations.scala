03b46bbb5a458e157253678497492fad197f132e
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-04-07 07:51:53 +0400

    Paragraph_String_Operations

--- src/main/scala/cp0/Paragraph_String_Operations.scala
+++ src/main/scala/cp0/Paragraph_String_Operations.scala
@@ -3,6 +3,7 @@ package cp0
 
 object Paragraph_String_Operations extends App {
   var str: String = "Hello, World!, I am a string and I am a sequence of characters"
+  println()
   println(str)
   println("Length of the string: " + str.length)
   println("First character of the string: " + str(0))
@@ -19,5 +20,13 @@ object Paragraph_String_Operations extends App {
   println("Index of the last occurrence of 'a' before 10th character: " + str.lastIndexOf('a', 10))
   println("Index of the first occurrence of 'a' after 10th character: " + str.indexOfSlice("am", 10))
   println("Index of the last occurrence of 'a' before 10th character: " + str.lastIndexOfSlice("am", 10))
-
+  println("String contains 'World': " + str.contains("World"))
+  println("Split the string by ',': " + str.split(",").mkString("Array(", ", ", ")"))
+  println("Starts with 'Hello': " + str.startsWith("Hello"))
+  println("Ends with 'characters': " + str.endsWith("characters"))
+  println("Replace 'a' with 'A': " + str.replace('a', 'A'))
+  println("Replace 'a' with 'A' only first 2 occurrences: " + str.replaceFirst("a", "A"))
+  println("Replace 'a' with 'A' all occurrences: " + str.replaceAll("a", "A"))
+  println("Lower case: " + str.toLowerCase)
+  println("Upper case: " + str.toUpperCase)
 }
