97a4970a5e53e8f39ae61fd88834f539e5a7b92a
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-04-07 08:11:49 +0400

    Paragraph_String_Operations

---
+++ src/main/scala/cp0/Paragraph_OOP.scala
@@ -0,0 +1,14 @@
+package com.intellibucket.lessons
+package cp0
+
+object Paragraph_OOP extends App {
+
+    // OOP - Object Oriented Programming
+
+    // Class - blueprint for creating objects
+
+    class Person {
+      var name: String = "John Doe"
+      var age: Int = 0
+    }
+}
