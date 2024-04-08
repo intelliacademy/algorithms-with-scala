a5a71d7c297b143a59173402025ff4020db024dd
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-04-08 06:28:44 +0400

    Paragraph_String_Operations

--- src/main/scala/cp0/Paragraph_OOP.scala
+++ src/main/scala/cp0/Paragraph_OOP.scala
@@ -1,19 +1,55 @@
 package com.intellibucket.lessons
 package cp0
 
+import java.util.UUID
+import scala.annotation.targetName
+import scala.language.postfixOps
+
 object Paragraph_OOP extends App {
 
-    val person1 = new Person
-    var person2 = Person()
-    var person3 = Person("John Doe", 30)
-    var person4 = new Person("Jane Doe", 25)
-    person4.age = 26
-    //person3.name = "Jane Doe"
+    val task1 = new Task
+    var task2 = Task()
+    var task3 = Task("New", 1)
+    var task4 = new Task("Lin", 5)
+    task4.priority = 1
+    //task3.name = "Jane Doe"
+
+    task4 walkTo "the park"
+    task4()
+    (task4 + task3) walkTo "the park"
+    task1.+ (task3) walkTo "the park"
+
+  println(1.+(2))
+   println(!task2)
+   println(task2 isFree)
 
 
+  class Task(val name: String, var priority: Int) {
+    def this() = this("Task".concat("-").concat(UUID.randomUUID().toString), 5)
+    def this(name: String) = this(name, 5)
 
-  class Person(val name: String, var age: Int) {
-    def this() = this("John Doe", 30)
-    def this(name: String) = this(name, 30)
+    def walkTo(destination: String): Unit = {
+      println(s"$name is walking to $destination")
+    }
+
+    def apply(): Unit = {
+      println(s"Name: ${this.name}, Priority: ${this.priority}")
+    }
+
+    @targetName("concatTask")
+    def +(other: Task): Task = {
+      new Task(this.name.concat(" && ").concat(other.name), this.priority + other.priority)
+    }
+
+    def isFree: Boolean = {
+      true
+    }
+
+    @targetName("isNotFree")
+    def unary_! : Boolean = !isFree
+    
+
   }
+
+
 }
