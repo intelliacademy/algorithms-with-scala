77a36163fc1962ab9e9bbd192e07c25c91d15a7b
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-04-20 17:32:04 +0400

    Curved Functions

---
+++ src/main/scala/cp0/HigherOrderedFunctions_Playground.scala
@@ -0,0 +1,14 @@
+package com.intellibucket.lessons
+package cp0
+
+object HigherOrderedFunctions_Playground extends App {
+  println("Higher Ordered Functions Playground")
+  
+  // HOFs are functions that take other functions as arguments or return functions as results
+  // They are a powerful tool for abstraction and composition
+  
+  // Curries functions are a special case of HOFs
+  // They are functions that return other functions
+  
+  
+}
--- src/main/scala/cp0/HightOrderedFunctions_Playground.scala
+++
@@ -1,6 +0,0 @@
-package com.intellibucket.lessons
-package cp0
-
-object HightOrderedFunctions_Playground extends App {
-  
-}
