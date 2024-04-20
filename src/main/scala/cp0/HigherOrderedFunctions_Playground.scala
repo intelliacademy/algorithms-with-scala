eba927a10052e8c905a830ac91cce9712b795c5a
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-04-20 18:16:39 +0400

    Curved Functions

--- src/main/scala/cp0/HigherOrderedFunctions_Playground.scala
+++ src/main/scala/cp0/HigherOrderedFunctions_Playground.scala
@@ -1,14 +1,25 @@
 package com.intellibucket.lessons
 package cp0
 
+import scala.annotation.tailrec
+
 object HigherOrderedFunctions_Playground extends App {
   println("Higher Ordered Functions Playground")
-  
+
   // HOFs are functions that take other functions as arguments or return functions as results
   // They are a powerful tool for abstraction and composition
-  
+
   // Curries functions are a special case of HOFs
   // They are functions that return other functions
-  
-  
+
+//  var function0: (Int,(String,(Int => Int))) => (Int => Int) = ??? // This is a curried function
+  //var function1: (Int,(String,(Int => Int))) => Int = (a,b) => b._2(b._1)
+
+  @tailrec
+  private def nTimesExecutingFunction(f: Int => Int, n: Int, x: Int): Int = {
+    if (n <= 0) x
+    else nTimesExecutingFunction(f, n - 1, f(x))
+  }
+
+  println(nTimesExecutingFunction(x => x + 1, 10, 1))
 }
