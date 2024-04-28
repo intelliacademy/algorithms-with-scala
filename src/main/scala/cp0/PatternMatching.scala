2efd51e6cca3e001cc736457923ea15b9b3cfef9
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-04-28 17:07:38 +0400

    PatternMatching

--- src/main/scala/cp0/PatternMatching.scala
+++ src/main/scala/cp0/PatternMatching.scala
@@ -28,4 +28,36 @@ object PatternMatching extends App {
     case _ => println(s"Unknown person")
   }
 
+
+  sealed class Animal
+  case class Dog(name: String) extends Animal
+  case class Cat(name: String) extends Animal
+  case class Bird(name: String) extends Animal
+
+  val animal: Animal = Dog("Buddy")
+
+  animal match {
+    case Dog(name) => println(s"Dog name is $name")
+    case Cat(name) => println(s"Cat name is $name")
+    case _ => println("Unknown animal")
+  }
+
+
+  trait Expreression
+  case class Number(value: Int) extends Expreression
+  case class Sum(e1: Expreression, e2: Expreression) extends Expreression
+  case class Prod(e1: Expreression, e2: Expreression) extends Expreression
+
+  def eval(e: Expreression): Int = e match {
+    case Number(value) => value
+    case Sum(e1, e2) => eval(e1) + eval(e2)
+    case Prod(e1, e2) => {
+      def evalProd(e: Expreression): Int = e match {
+        case Number(value) => value
+        case Prod(e1, e2) => evalProd(e1) * evalProd(e2)
+        case _ => throw new IllegalArgumentException("Invalid expression")
+      }
+      evalProd(e1) * evalProd(e2)
+    }
+  }
 }
