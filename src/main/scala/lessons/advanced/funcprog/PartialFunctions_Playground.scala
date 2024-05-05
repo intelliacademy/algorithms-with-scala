599ac13cfb5a1656d0a94d6d558b5bab1c73d22f
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-05-05 16:53:43 +0400

    Advanced Scala Lesson Partial Functions

--- src/main/scala/lessons/advanced/funcprog/PartialFunctions_Playground.scala
+++ src/main/scala/lessons/advanced/funcprog/PartialFunctions_Playground.scala
@@ -1,6 +1,8 @@
 package com.intellibucket.lessons
 package lessons.advanced.funcprog
 
+import scala.util.Try
+
 object PartialFunctions_Playground extends App {
   println("Partial Functions")
 
@@ -9,4 +11,15 @@ object PartialFunctions_Playground extends App {
   var cFunction :Function1[Int,Int] = x => x + 1
 
 
+  val aPartialFunction: PartialFunction[Int,Int] = {
+    case 1 => 10
+    case 2 => 20
+    case 3 => 30
+  }
+
+  val result:Int = Try[Int]{
+    aPartialFunction(99)
+  }.getOrElse(990)
+
+  println(result)
 }
