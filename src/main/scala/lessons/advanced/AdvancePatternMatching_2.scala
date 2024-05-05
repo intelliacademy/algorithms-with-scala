33224df833e26ca3f1423cf05981494f7130a4cc
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-05-05 12:27:26 +0400

    Advanced Scala Lesson Pattern Matching

--- src/main/scala/lessons/advanced/AdvancePatternMatching_2.scala
+++ src/main/scala/lessons/advanced/AdvancePatternMatching_2.scala
@@ -3,6 +3,31 @@ package lessons.advanced
 
 object AdvancePatternMatching_2 extends App {
   println("Advance Pattern Matching 2")
+  println()
+
+  var singleItemList = List(1)
+  var twoItemList = List(1,2)
+  var multiItemList = List(1,2,3,4,5)
+
+  var result = singleItemList match
+    case head :: Nil => s"This is single item list"
+    case _ => s"This is multi item list"
+
+  //println(result)
 
 
+  var result2 = twoItemList match
+    case head :: Nil => s"This is single item list"
+    case head :: 2 :: Nil => s"This is only two item list"
+    case _ => s"This is multi item list"
+
+  println(result2)
+
+  def calculateLength[A](list: List[A]):Int = {
+    list match
+      case Nil => 0
+      case head :: tail => 1 + calculateLength(tail)
+  }
+
+  println(calculateLength(multiItemList))
 }
