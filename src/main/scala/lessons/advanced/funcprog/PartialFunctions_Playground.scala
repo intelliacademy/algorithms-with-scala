3106fe6a10de3d58be7a02c9269c345da1a5a4d5
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-05-05 17:04:44 +0400

    Advanced Scala Lesson Partial Functions

--- src/main/scala/lessons/advanced/funcprog/PartialFunctions_Playground.scala
+++ src/main/scala/lessons/advanced/funcprog/PartialFunctions_Playground.scala
@@ -10,6 +10,12 @@ object PartialFunctions_Playground extends App {
   var bFunction: (Int) => Int = x => x + 1
   var cFunction :Function1[Int,Int] = x => x + 1
 
+  val aNiceFussyFunction = (x: Int) => x match
+    case 3 => 30
+
+  val bNiceFussyFunction: Int => Int = (x: Int) => x match
+    case 3 => 30
+
 
   val aPartialFunction: PartialFunction[Int,Int] = {
     case 1 => 10
@@ -22,4 +28,17 @@ object PartialFunctions_Playground extends App {
   }.getOrElse(990)
 
   println(result)
+  println(aPartialFunction.isDefinedAt(99))
+  var liftedFunc = aPartialFunction.lift
+  println(s"lifted value 1 match ${liftedFunc(1).getOrElse(100)}")
+  println(s"lifted value 99 match ${liftedFunc(99).getOrElse(101)}")
+
+  var chainPFunction = aPartialFunction.orElse[Int,Int]({
+    case 99 => {
+      println("Or Else function executing")
+      990
+    }
+  })
+
+  println(s"Or Else Partial Function ${chainPFunction(99)}")
 }
