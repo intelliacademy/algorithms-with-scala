dad7ca23aecb23310d18c84e23eaaf5151b60a02
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-05-05 18:07:57 +0400

    Advanced Scala Lesson Function Set

--- src/main/scala/lessons/advanced/funcprog/FunctionsSet_Playground.scala
+++ src/main/scala/lessons/advanced/funcprog/FunctionsSet_Playground.scala
@@ -13,13 +13,13 @@ object FunctionsSet_Playground extends App {
     def contains(a: A): Boolean
     def +(a: A): MySet[A]
     def ++[B <: A](other: MySet[B]): MySet[A]
-    def map[B,C <: A](f: C => B): MySet[B]
+    def map[B](f: A => B): MySet[B]
     def flatMap[B,C <: A](f: C => MySet[B]): MySet[B]
     def filter[B <: A](filter: B => Boolean): MySet[B]
     def foreach[B <: A](consumer: B => Unit): Unit
   end MySet
 
-  class ConsSet[A]() extends MySet[A]:
+  class ConsSet[A](val a:A, mySet: MySet[A]) extends MySet[A]:
     override def +(a: A): MySet[A] = ???
 
     override def flatMap[B, C <: A](f: C => MySet[B]): MySet[B] = ???
@@ -28,7 +28,7 @@ object FunctionsSet_Playground extends App {
 
     override def foreach[B <: A](consumer: B => Unit): Unit = ???
 
-    override def map[B, C <: A](f: C => B): MySet[B] = ???
+    override def map[B](f: A => B): MySet[B] = ???
 
     override def filter[B <: A](filter: B => Boolean): MySet[B] = ???
 
@@ -37,18 +37,18 @@ object FunctionsSet_Playground extends App {
 
   object ConsSet
 
-  object EmptyMySet extends MySet[Nothing]:
-    override def contains(a: Nothing): Boolean = ???
+  class EmptyMySet extends MySet[Nothing]:
+    override def contains(a: Nothing): Boolean = false
 
-    override def +(a: Nothing): MySet[Nothing] = ???
+    override def +(a: Nothing): MySet[Nothing] = new ConsSet[Nothing](a,this)
 
-    override def ++[B <: Nothing](other: MySet[B]): MySet[Nothing] = ???
+    override def ++[B <: Nothing](other: MySet[B]): MySet[Nothing] = other
 
-    override def map[B, C <: Nothing](f: C => B): MySet[B] = ???
-
+    override def map[B](f: Nothing => B): MySet[B] = ???
+    
     override def flatMap[B, C <: Nothing](f: C => MySet[B]): MySet[B] = ???
 
-    override def foreach[B <: Nothing](consumer: B => Unit): Unit = ???
+    override def foreach[B <: Nothing](consumer: B => Unit): Unit = ()
 
     override def filter[B <: Nothing](filter: B => Boolean): MySet[B] = ???
   end EmptyMySet
