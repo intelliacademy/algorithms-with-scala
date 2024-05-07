3dff571179f008d878cdd94bacd6a4c76ce54c81
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-05-08 01:05:55 +0400

    Advanced Scala Lesson Function Set

--- src/main/scala/lessons/advanced/funcprog/FunctionsSet_Playground.scala
+++ src/main/scala/lessons/advanced/funcprog/FunctionsSet_Playground.scala
@@ -1,7 +1,7 @@
 package com.intellibucket.lessons
 package lessons.advanced.funcprog
 
-import scala.annotation.tailrec
+import scala.annotation.{tailrec, targetName}
 import scala.language.postfixOps
 
 object FunctionsSet_Playground extends App {
@@ -12,26 +12,38 @@ object FunctionsSet_Playground extends App {
   var values = MySet(1, 2, 3, 4, 5)
   values + 5 ++ MySet(1,2,3) + 4 + 4 map (x =>x * 3) filter (x => x % 2 != 0) flatMap (x => MySet(x + 1,x + 2)) foreach println
 
+  //println(values contains 1)
+  values - 1
+  println(values contains 1)
 }
 trait MySet[A] extends (A => Boolean):
   override def apply(v1: A): Boolean = contains(v1)
   def contains(value: A): Boolean
+  @targetName("add")
   def +(value: A): MySet[A]
+  @targetName("concat")
   def ++(other: MySet[A]): MySet[A]
   def map[B](f: A => B): MySet[B]
   def flatMap[B](f: A => MySet[B]): MySet[B]
   def filter(filter: A => Boolean): MySet[A]
   def foreach(consumer: A => Unit): Unit
+  @targetName("remove")
+  def -(value: A) : MySet[A]
+  @targetName("intersection")
+  def &(other: MySet[A]) : MySet[A]
+  @targetName("difference")
+  def --(other: MySet[A]) : MySet[A]
 end MySet
 
 
 class ConsSet[A](val head:A, tail: MySet[A]) extends MySet[A]:
 
+  @targetName("add")
   override def +(value: A): MySet[A] =
     if (this contains value) this
     else ConsSet(value,this)
   end +
-
+  
 
   override def flatMap[B](f: A => MySet[B]): MySet[B] = (this.tail flatMap f) ++ f(this.head)
 
@@ -42,6 +54,7 @@ class ConsSet[A](val head:A, tail: MySet[A]) extends MySet[A]:
   [] ++ [4,5,6] + 3 + 2 + 1 ===>
   [1,2,3,4,5,6]
    */
+  @targetName("concat")
   override def ++(other: MySet[A]): MySet[A] = this.tail ++ other + this.head
 
   override def foreach(consumer: A => Unit): Unit =
@@ -60,6 +73,25 @@ class ConsSet[A](val head:A, tail: MySet[A]) extends MySet[A]:
 
 
   override def contains(value: A): Boolean = value == head || (tail contains value)
+
+  @targetName("remove")
+  override def -(value: A): MySet[A] = {
+    @tailrec def removeElement(bef: A, tail: MySet[A]): MySet[A] = {
+      if (this.head == value) {
+        this.tail + bef
+      }else{
+        removeElement(this.head,this.tail);
+      }
+    }
+    removeElement(this.head,this.tail)
+  }
+
+  @targetName("intersection")
+  override def &(other: MySet[A]): MySet[A] = ???
+
+  @targetName("difference")
+  override def --(other: MySet[A]): MySet[A] = ???
+
 
 end ConsSet
 
@@ -82,10 +114,21 @@ end MySet
 
 class EmptyMySet[A] extends MySet[A]:
 
+  @targetName("remove")
+  override def -(value: A): MySet[A] = new EmptyMySet[A]
+
+  @targetName("difference")
+  override def --(other: MySet[A]): MySet[A] = this
+
+  @targetName("intersection")
+  override def &(other: MySet[A]): MySet[A] = this
+
   override def contains(a: A): Boolean = false
 
+  @targetName("add")
   override def +(a: A): MySet[A] = new ConsSet[A](a,this)
 
+  @targetName("concat")
   override def ++(other: MySet[A]): MySet[A] = other
 
   override def map[B](f: A => B): MySet[B] = new EmptyMySet[B]
