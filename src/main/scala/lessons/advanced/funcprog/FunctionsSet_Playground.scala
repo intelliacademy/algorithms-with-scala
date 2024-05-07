aff7732ccdaed26d41eba45eccbe4e9d044401ad
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-05-07 23:46:16 +0400

    Advanced Scala Lesson Function Set

--- src/main/scala/lessons/advanced/funcprog/FunctionsSet_Playground.scala
+++ src/main/scala/lessons/advanced/funcprog/FunctionsSet_Playground.scala
@@ -22,11 +22,13 @@ object FunctionsSet_Playground extends App {
   end MySet
 
 
-  class ConsSet[A](val head:A, tail: MySet[A]) extends MySet[A]:
+  private class ConsSet[A](val head:A, tail: MySet[A]) extends MySet[A]:
 
     override def +(value: A): MySet[A] =
       if (this contains value) this
       else ConsSet(value,this.tail)
+    end +
+
 
     override def flatMap[B](f: A => MySet[B]): MySet[B] = (this.tail flatMap f) ++ f(this.head)
 
@@ -34,7 +36,7 @@ object FunctionsSet_Playground extends App {
     [1,2,3] ++ [4,5,6] =
     [1,2] ++ [4,5,6] + 3 =>
     [1] ++ [4,5,6] + 3 + 2 =>
-    [] ++ [4,5,6] + 3 + 2 + 1 =>
+    [] ++ [4,5,6] + 3 + 2 + 1 ===>
     [1,2,3,4,5,6]
      */
     override def ++(other: MySet[A]): MySet[A] = this.tail ++ other + this.head
@@ -42,33 +44,37 @@ object FunctionsSet_Playground extends App {
     override def foreach(consumer: A => Unit): Unit =
       consumer(head)
       tail foreach consumer
+    end foreach
 
-    //Reconstruction
+
     override def map[B](f: A => B): MySet[B] = (this.tail map f) + f(this.head)
 
-    override def filter(predicate: A => Boolean): MySet[A]  = {
+    override def filter(predicate: A => Boolean): MySet[A]  =
       val filteredTail = this.tail filter predicate
       if (predicate(head)) then (filteredTail + head)
       filteredTail
-    }
+    end filter
+
 
     override def contains(a: A): Boolean = a == this.head || this.contains(a)
 
   end ConsSet
 
-  object ConsSet:
+  private object ConsSet:
     def apply[A](a: A, mySet: MySet[A]) : MySet[A] = new ConsSet[A](a,mySet)
-    def apply[A](args: A*) : MySet[A] = {
-      @tailrec def build(valSeq: Set[A], acc: MySet[A]): MySet[A] = {
+
+    def apply[A](args: A*) : MySet[A] =
+      @tailrec def build(valSeq: Set[A], acc: MySet[A]): MySet[A] =
         if (valSeq.isEmpty) then return acc
         build(valSeq.tail,acc + valSeq.head)
-      }
+      end build
       build(args.toSet,new EmptyMySet[A])
-    }
+    end apply
+
   end ConsSet
 
 
-  class EmptyMySet[A] extends MySet[A]:
+  private class EmptyMySet[A] extends MySet[A]:
 
     override def contains(a: A): Boolean = false
 
@@ -84,6 +90,9 @@ object FunctionsSet_Playground extends App {
 
     override def filter(filter: A => Boolean): MySet[A] = this
 
+  end EmptyMySet
+
+  private object EmptyMySet:
   end EmptyMySet
 
 
